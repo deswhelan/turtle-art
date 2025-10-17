75b944fc39a973878847ec2717cd424c5fa5d7fc
Des Whelan <wheland6@tcd.ie>
2025-10-17 16:29:33 +0100

    centre dot painting and refactor

--- main.py
+++ main.py
@@ -19,19 +19,29 @@ def get_colour_palette():
         colour_palette.append(color.rgb)
     return colour_palette
 
-def print_row_of_dots(row_length, colour_palette):
+def print_row_of_dots(dot_size, step_size, row_length, colour_palette):
     for _ in range(row_length):
-        donatello.dot(20, random.choice(colour_palette))
-        donatello.fd(70)
+        donatello.dot(dot_size, random.choice(colour_palette))
+        donatello.fd(step_size)
 
-def paint_dot_painting(row_count, column_count):
-    # TODO: stretch - centre the painting
+def paint_dot_painting(dot_size, gap_size, row_count, column_count):
+    step_size = dot_size + gap_size
+
+    # offset position of first dot so that painting is centred on screen
+    dot_1_x_pos = -(((column_count / 2) * step_size) - (step_size / 2))
+    dot_1_y_pos = -(((row_count / 2) * step_size) - (step_size / 2))
+    donatello.setpos(dot_1_x_pos, dot_1_y_pos)
+
     colour_palette = get_colour_palette()
-    for row_number in range(row_count):
-        print_row_of_dots(column_count, colour_palette)
-        donatello.home()
-        donatello.setpos(0, (70 * (row_number + 1)))
 
-paint_dot_painting(10, 10)
+    for row_number in range(row_count):
+        print_row_of_dots(dot_size, step_size, column_count, colour_palette)
+        donatello.home()
+        donatello.setpos(dot_1_x_pos, (dot_1_y_pos + (step_size * (row_number + 1))))
+
+    donatello.home()
+    donatello.hideturtle()
+
+paint_dot_painting(20, 50, 10, 10)
 
 screen.exitonclick()
